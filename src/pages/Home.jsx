9b56ab9e00288d14086303fd83de087a7c885809
Dogukan Caltekin <dcaltekin@gmail.com>
2022-06-28 02:15:55 +0300

    page-loading-and-button-size

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -37,7 +37,7 @@ function Home() {
         {lastPage && !isLoading && (
           <button
             onClick={() => dispatch(axiosCharacters(page))}
-            className="text-3xl inline-block px-12 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out"
+            className="text-3xl inline-block px-12 py-2.5 bg-red-600 text-white leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out"
           >
             Daha Fazla -{page}-
           </button>
